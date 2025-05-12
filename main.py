a31e0a6382a2c0b8455b0f155fefafd67cced8e1
denielbreid777 <danielshkyndia@gmail.com>
2025-05-11 23:10:34 -0500

    Random Joke

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from flask import Flask, render_template, request
+import requests
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html", joke=requests.get("https://api.chucknorris.io/jokes/random"))
+
+
+
+
+
+
+app.run(debug=True)
