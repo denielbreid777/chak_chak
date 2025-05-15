b3723542ff7a2763fc31f83687f11ebdef10e494
denielbreid777 <danielshkyndia@gmail.com>
2025-05-14 20:01:16 -0500

    Count Get and Post Zapross

--- main.py
+++ main.py
@@ -6,11 +6,21 @@ from random import randint
 app = Flask(__name__)
 
 
+total_get = 0
+total_post = 0
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    response = make_response(render_template("index.html", joke=requests.get("https://api.chucknorris.io/jokes/random")))
+    global total_get, total_post, home_visits
+
+    # GET/POST підщот
+    if request.method == "GET":
+        total_get += 1
+    elif request.method == "POST":
+        total_post += 1
+
+    response = make_response(render_template("index.html", joke=requests.get("https://api.chucknorris.io/jokes/random"), get=total_get, post=total_post))
     
     if  request.cookies.get("id"):
         response.set_cookie("bal",  str(int(request.cookies.get("bal")) + 1), httponly=True)
